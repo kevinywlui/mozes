59376573b73d8d7d03dcb2640fc5bf1661db48d7
Kevin Lui <kevinywlui@gmail.com>
2015-05-31 23:15:48 -0700

    affine

---
+++ mozes.py
@@ -0,0 +1,52 @@
+class Mozes:
+
+    def __init__(self,cartantype):
+        self.cartantype = cartantype
+        self.n = len(self.cartantype.index_set())
+        self.state = [1] * len(self.cartantype.index_set())
+        self.identity = [1] * self.n
+
+        # R[i] is list of length i words
+        # run computeR(L) to compute up to R[L]
+        self.R=[[self.identity],[]]
+
+        # stack is the set of elements that need fired upon
+        self.stack=[[self.identity,0]]
+
+    def fire(self,state,k):
+        # tranpose to agree with Humphreys
+        v = self.cartantype.cartan_matrix().transpose()[k]
+        newstate = list(state)
+        for i in range(self.n):
+            if i == k:
+                pass
+            else:
+                newstate[i] = state[i] + abs(v[i])*state[k]
+
+        newstate[k] = -state[k]
+        return newstate
+
+    def computeR(self,L):
+        while len(self.stack) > 0 and (self.stack[-1][1] <= L or L==-1):
+            element = self.stack.pop()
+            l = element[1]
+
+            if l+2 > len(self.R):
+                self.R.append([])
+
+            for k in range(self.n):
+                new = self.fire(element[0],k)
+                if element[1] == 0:
+                    self.R[1].append(new)
+                    self.stack.insert(0,[new,1])
+                elif new not in self.R[l-1] and new not in self.R[l+1]:
+                    # If w is length l then sw is length l\pm 1
+                    self.R[l+1].append(new)
+                    self.stack.insert(0,[new,l+1])
+                else:
+                    pass #repeated element
+
+    def order(self):
+        self.computeR(-1)
+        S=[item for sublist in self.R for item in sublist]
+        return len(S)
--- test.sage
+++ test.sage
@@ -2,7 +2,6 @@ from mozes import Mozes
 
 def main():
     tests = [['A',3],['B',4],['F',4]]
-
     for test in tests:
         A = Mozes(CartanType(test))
         m = A.order()
@@ -14,5 +13,12 @@ def main():
         else:
             print("Disagreement on "+str(test))
 
+    affines = [[['A',2,1],2],[['D',5,2],1]]
+    for affine in affines:
+        A = Mozes(CartanType(affine[0]))
+        A.computeR(affine[1])
+        k = len(A.R[affine[1]])
+        print("Number of length "+str(affine[1]) \
+                +" elements in "+str(affine[0])+": "+str(k))
 if __name__ == '__main__':
     main()
